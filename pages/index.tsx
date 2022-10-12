3fe20e9b34704f29173878c486d7e8290d1ba9e4
cheeter_Lee <johnson991210@gmail.com>
2022-10-12 16:17:16 +0800

    modified client.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
+
+// interface Iprops {
+//   videos: 
+// }
 
 const Home: NextPage = () => {
+  // console.log(videos)
+  
+
   return (
     <h1 className='text-3xl font-bold underline'>
       Hello world!
     </h1>
   )
+}
+
+export const getSeverSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`)
+  console.log(response)
+  
+  // return {
+  //   props: {
+  //     videos: data
+  //   }
+  // }
+  return {
+    props: {}
+  }
 }
 
 export default Home
